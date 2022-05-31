c7941994dff6c8c922a09bbbdf52e6c2ea7a16a9
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-05-31 22:50:50 +0900

    szpp

---
+++ bacha/bacha.2022.05.30/b1.cpp
@@ -0,0 +1,30 @@
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+#define reps(i, s, n) for (long long i = (long long)(s); i < (long long)(n); ++i)
+#define rrep(i, n) for (long long i = (long long)(n-1); i >= 0; --i)
+#define MOD 1000000007
+#define INF 2000000000
+#define INFL 1000000000000000000
+template <typename T> bool chmin(T &a, const T& b) { if (a > b) { a = b; return true; } return false; }
+template <typename T> bool chmax(T &a, const T& b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> long long count_digit(T n) { long long res = 0; while (n > 0) { ++res; n /= 10; } return res; }
+typedef long long ll;
+#include <bits/stdc++.h>
+using namespace std;
+
+void correct(string s1, string s2) {
+    if (s1 == s2) {
+        cout << "IDENTICAL" << endl;
+        return;
+    }
+}
+int main() {
+    while (true) {
+        string s1, s2;
+        cin >> s1;
+        if (s1 == ".") break;
+        else cin >> s2;
+        correct(s1, s2);
+    }
+
+    return 0;
+}
---
+++ bacha/bacha.2022.05.30/c1.cpp
@@ -0,0 +1,24 @@
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+#define reps(i, s, n) for (long long i = (long long)(s); i < (long long)(n); ++i)
+#define rrep(i, n) for (long long i = (long long)(n-1); i >= 0; --i)
+#define MOD 1000000007
+#define INF 2000000000
+#define INFL 1000000000000000000
+template <typename T> bool chmin(T &a, const T& b) { if (a > b) { a = b; return true; } return false; }
+template <typename T> bool chmax(T &a, const T& b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> long long count_digit(T n) { long long res = 0; while (n > 0) { ++res; n /= 10; } return res; }
+typedef long long ll;
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    vector<int> w(n);
+    int dp[101][11][3];
+}
+int main() {
+    
+    return 0;
+}
