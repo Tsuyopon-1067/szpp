11277944eec712d70ec378c859d6b737bb186774
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-06-14 13:57:23 +0900

    szpp

---
+++ bacha/bacha.2022.06.13/1.cpp
@@ -0,0 +1,29 @@
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+#define reps(i, s, n) for (long long i = (long long)(s); i < (long long)(n); ++i)
+#define rrep(i, n) for (long long i = (long long)(n-1); i >= 0; --i)
+#define MOD 1000000007
+#define INF 2000000000
+#define INFL 1000000000000000000
+template <typename T> bool chmin(T &a, const T& b) { if (a > b) { a = b; return true; } return false; }
+template <typename T> bool chmax(T &a, const T& b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> long long count_digit(T n) { long long res = 0; while (n > 0) { ++res; n /= 10; } return res; }
+typedef long long ll;
+#include <bits/stdc++.h>
+using namespace std;
+
+void calc(string s) {
+    vector<string> t;
+    for (auto c : s) {
+        
+    }
+}
+int main() {
+    long long n;
+    cin >> n;
+    for (long long i = 0; i < n; ++i) {
+        string s;
+        cin >> s;
+        calc(s);
+    }
+    return 0;
+}
